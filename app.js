649596f8696efea8d459066c229d19eb26695aa4
jsm13 <john.s.master@gmail.com>
2018-09-19 10:39:53 -0400

    Add GHE sign in

---
+++ app.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+
+const passport = require('passport');
+const GitHubStrategy = require('passport-github');
+
+app.use(passport.initialize());
+
+const GHE_BASE_URL = 'https://git.generalassemb.ly';
+passport.use(new GitHubStrategy({
+  clientID: process.env.GHE_CLIENT_ID,
+  clientSecret: process.env.GHE_CLIENT_SECRET,
+  callbackURL: 'http://localhost:3000/auth/ghe/callback',
+  userProfileURL: GHE_BASE_URL + '/api/v3/user',
+  authorizationURL: GHE_BASE_URL + '/login/oauth/authorize',
+  tokenURL: GHE_BASE_URL + '/login/oauth/access_token'
+},
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile)
+    cb(undefined, profile);
+  }
+));
+
+app.get('/login', (req, res) => {
+  res.send('<a href="/auth/github">Sign in with GitHub</a>')
+});
+
+app.get('/auth/github', passport.authenticate('github', { session: false }));
+
+app.get('/auth/ghe/callback',
+        passport.authenticate('github', { failureRedirect: '/login',  session: false  }),
+  (req, res) => {
+    res.redirect('/login');
+  }
+);
+
+
+app.listen(3000, () => {
+  console.log(`listening on port ${PORT}`)
+})
